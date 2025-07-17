e031d08b56699ed5a21a1656a28ec0b720324e39
Prakhar Bhardwaj <prakharbhardwaj021@gmail.com>
2025-07-17 22:16:23 +0530

    Project added

---
+++ unchoicedd-react-app/src/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: {}, // This line tells PostCSS to use the Tailwind CSS plugin
+    autoprefixer: {}, // This line tells PostCSS to use the Autoprefixer plugin
+  },
+};
